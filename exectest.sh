187bdaa9ee85adabc2c59378c1f220e80186f621
Ralox <dbarnett@liquidweb.com>
2013-12-12 17:22:04 -0500

    add bashparams & exectest.sh

---
+++ exectest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+
+echo "The script starts now."
+
+echo "Current User: $USER"
+echo
+
+echo "List of Users:"
+w
+echo
+
+echo "Setting color and value variables"
+COLOR="gold"
+VALUE="1000"
+
+echo "COLOR: $COLOR"
+echo "VALUE: $VALUE"
+
+echo "End of Script."
+echo
